996ca5487c87f03dc0ee83c1575e5e295a1b78bf
joker <1932728033@qq.com>
2020-02-13 17:34:43 +0800

    no message

--- electron.js
+++ electron.js
@@ -19,7 +19,7 @@ function createWindow () {
         }
     })
     // and load the index.html of the app.
-    mainWindow.loadFile(`${__dirname}\\dist\\index.html`)
+  mainWindow.loadFile(`${__dirname}\\dist\\index.html`)
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
